9e53c5102cc6282a4a527a2c913282f5213ffe3a
marcelsafont <marcel.safont1983@gmail.com>
2023-02-27 11:29:42 +0100

    importer and rollback

--- importer.js
+++ importer.js
@@ -5,21 +5,31 @@ import * as fs from 'fs';
 import { parse } from 'csv-parse';
 
 // console.log(process.env.API_TOKEN)
-
 if(!process.env.API_TOKEN || !process.env.BLOG_ID || !process.env.SITE_SLUG || !process.env.NATION_SLUG){
   console.log('Some required enviroment variable is not set')
+}
+else if(fs.existsSync("./imports.json")){
+  console.log('launch rollback before need import')
 }else{
   getCsvData()
 }
 
-function getCsvData(){
-  fs.createReadStream("./blog.csv")
-  .pipe(parse({ columns: true , bom: true}))
-  .on("data", async (row) => {
+async function getCsvData(){
+  const records = []
+  const parser = fs
+  .createReadStream("./blog.csv")
+  .pipe(parse({
+     columns: true , bom: true
+  }));
+  for await (const row of parser) {
     const slug = row.name.split(' ').join('_').replace(/[^a-z0-9_]/gi,'').toLowerCase();
     const request = await fetch(`https://${process.env.NATION_SLUG}.nationbuilder.com/api/v1/sites/${process.env.SITE_SLUG}/pages/blogs/${process.env.BLOG_ID}/posts?access_token=${process.env.API_TOKEN}`, {method: 'POST', body:`{"blog_post": {"name": "${row.name}", "status":"published", "slug": "${slug}", "content_after_flip": "${row.content}"}}`, headers: {'Content-Type': 'application/json'}})
     const response = await request.json();
-    
-  })
+    records.push({id: response.blog_post.id})
+  }
+  let data = await JSON.stringify(records);
+  fs.writeFileSync('imports.json', data);
+  console.log('all imported')
+  return
 }
 
--- rollback.js
+++ rollback.js
@@ -1,1 +1,32 @@
-// todo generate rollback code based on saved previous ids
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config()
+import fetch from 'node-fetch';
+import * as fs from 'fs';
+
+
+if(!process.env.API_TOKEN || !process.env.BLOG_ID || !process.env.SITE_SLUG || !process.env.NATION_SLUG){
+  console.log('Some required enviroment variable is not set')
+}
+else if(!fs.existsSync("./imports.json")){
+  console.log('nothing to rollback')
+}else{
+  rollBackContent()
+}
+
+async function rollBackContent(){
+  fs.readFile('./imports.json', (err, data) => {
+    if (err) throw err;
+    let row = JSON.parse(data);
+    for (const item of row) {
+      removeItems(item.id)
+    }
+    fs.rmSync('./imports.json', {
+      force: true,
+    });
+  });
+}
+
+async function removeItems(id){
+  const request = await fetch(`https://${process.env.NATION_SLUG}.nationbuilder.com/api/v1/sites/${process.env.SITE_SLUG}/pages/blogs/${process.env.BLOG_ID}/posts/${id}/?access_token=${process.env.API_TOKEN}`, {method: 'DELETE', headers: {'Content-Type': 'application/json'}})
+  // todo show response result
+}
