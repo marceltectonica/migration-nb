e4c432721c16c7c27c3df7531c98b076b210a9c3
marcelsafont <marcel.safont1983@gmail.com>
2023-02-27 15:26:05 +0100

    remove console logs

--- importer.js
+++ importer.js
@@ -22,10 +22,8 @@ async function getCsvData(){
      columns: true , bom: true
   }));
   for await (const row of parser) {
-    console.log(row)
     const slug = row.name.split(' ').join('_').replace(/[^a-z0-9_]/gi,'').toLowerCase();
     try{
-
       const request = await fetch(`https://${process.env.NATION_SLUG}.nationbuilder.com/api/v1/sites/${process.env.SITE_SLUG}/pages/blogs/${process.env.BLOG_ID}/posts?access_token=${process.env.API_TOKEN}`, {method: 'POST', body:`{"blog_post": 
         {"name": "${row.name}", 
         "status":"published", 
@@ -37,11 +35,10 @@ async function getCsvData(){
       headers: {'Content-Type': 'application/json'}})
       
       const response = await request.json();
-      console.log(response)
       records.push({id: response.blog_post.id})
       let data = await JSON.stringify(records);
       fs.writeFileSync('imports.json', data);
-      console.log('all imported')
+  
     } catch(e){
       console.log(e)
     }
